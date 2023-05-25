f2d77c39423c3483868630aca6170762ba480c1a
EmanuelGalli <emanuel.galli@bootcamp-upgrade.com>
2023-05-25 16:41:06 +0200

    mapeo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Footer from "./core/footer/Footer";
 import { Nav } from './core/nav/Nav.core';
+import  FleetPages from './pages/fleet/FleetPages';
 
 function App() {
   return (
     <div className="App">
     <Nav/>
+
       <Routes>
+        <Route path='/fleet' element={<FleetPages/>} >  </Route>
       </Routes>
       <Footer/>
     </div>
--- src/core/nav/Nav.core.jsx
+++ src/core/nav/Nav.core.jsx
@@ -6,7 +6,7 @@ export const Nav= () => {
         <img src="./src/assets/fotos/logo.png" alt="logo" />
         <ul className="ul_nav">
             <li><Link to="/">🏠 HOME</Link></li>
-            <li><Link to="/flota">🚗 FLOTA DE COCHES</Link></li>
+            <li><Link to="/fleet">🚗 FLOTA DE COCHES</Link></li>
             <li><Link to="/gestion">📅 GESTIONAR RESERVA</Link></li>
             <li><Link to="/login">LOGIN</Link></li>
             <li><Link to="/signup">✏️ REGISTER</Link></li>
