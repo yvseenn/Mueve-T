d838a499993e607404a0bdc86294c712f3ad2f23
yaseen <127450106+yvseenn@users.noreply.github.com>
2023-05-25 09:33:29 +0200

    login/register

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home.pages";
 import Footer from "./core/footer/Footer";
 import { Nav } from './core/nav/Nav.core';
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
     <Nav/>
       <Routes>
-        <Route path="/" element={<Home/>} />
       </Routes>
       <Footer/>
     </div>
