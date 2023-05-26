e5c94ad220d329e4ba552429298003ecdec3e506
diegosanchez96 <diego.sanchez@bootcamp-upgrade.com>
2023-05-26 16:28:13 +0200

    maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import LoginPage from "./pages/login/Login.page";
 import { VehicleContext } from "./context/Users.context";
 import { useContext } from "react";
 import SignupPage from "./pages/signup/Signup.page";
-import  FleetPages from './pages/fleet/FleetPages';
+// import { Carrusel } from "./assets/carrusel/carrusel";
+// import  FleetPages from './pages/fleet/FleetPages';
+import Home from "./pages/home/Home.pages";
 
 function App() {
 
@@ -15,12 +17,11 @@ function App() {
   return (
     <div className="App">
     <Nav/>
-
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-
-        <Route path='/fleet' element={<FleetPages/>} >  </Route>
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/fleet' element={<FleetPages/>} >  </Route> */}
       </Routes>
       <Footer />
     </div>
